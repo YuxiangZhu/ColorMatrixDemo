0becd688daf014cb27e216a986dc7eadf824cd54
朱玉翔 <zhuyuxiang@shizhuang-inc.com>
2021-03-23 17:06:24 +0800

    Initial commit

---
+++ app/src/main/java/com/alex/colormatrixdemo/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.alex.colormatrixdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /**
+     * 选择照片
+     */
+    fun doGallery(view: View) {
+
+    }
+
+    /**
+     * 拍照
+     */
+    fun doTakePhoto(view: View) {
+
+    }
+}
